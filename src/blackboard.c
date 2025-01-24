7648e6f85fd381259248a0d7fa2276e3d49892cd
Gian Marco Balia <133429164+balgian@users.noreply.github.com>
2025-01-24 19:07:42 +0100

    Implemented a first game window interface

--- src/blackboard.c
+++ src/blackboard.c
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <ncurses.h>
 #include "macros.h"
 
@@ -40,22 +43,96 @@ int parser(int argc, char *argv[], int *read_fds, int *write_fds, pid_t *watchdo
     return EXIT_SUCCESS;
 }
 
-void resize_window(WINDOW **win) {
-    int new_height, new_width;
+int initialize_ncurses() {
+    // * Start curses mode
+    if (initscr() == NULL) {
+        return EXIT_FAILURE;
+    }
+    cbreak();               // * Disable line buffering
+    noecho();               // * Don't echo pressed keys
+    curs_set(FALSE);        // * Hide the cursor
+    start_color();          // * Enable color functionality
+    use_default_colors();   // * Allow default terminal colors
 
-    // Get the new terminal dimensions
-    getmaxyx(stdscr, new_height, new_width);
+    // * Initialize color pairs
+    init_pair(1, COLOR_BLUE, -1);    // * Drone
+    init_pair(2, COLOR_GREEN, -1);   // * Targets
+    init_pair(3, COLOR_YELLOW, -1);  // * Obstacles
 
-    // Ensure the window dimensions are positive and fit within the screen
-    if (*win != NULL) {
-        delwin(*win); // Delete the old window
+    return EXIT_SUCCESS;
+}
+
+void game_window(WINDOW **win, int *height, int *width, int *status, int *read_fds, int *write_fds) {
+    // * Remap the read and write file descriptors
+    const int keyboard = read_fds[0];
+    const int obstacle_read = read_fds[1];
+    const int target_read = read_fds[2];
+    const int dynamic_read = read_fds[3];
+
+    const int obstacle_write = write_fds[0];
+    const int target_write = write_fds[1];
+    const int dynamic_write = write_fds[2];
+
+    // * Set the keyboard commands not blocking
+    int flags = fcntl(keyboard, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl F_GETFL");
+        exit(EXIT_FAILURE);
+    }
+    flags |= O_NONBLOCK;
+    if (fcntl(keyboard, F_SETFL, flags) == -1) {
+        perror("fcntl F_SETFL");
+        exit(EXIT_FAILURE);
     }
 
-    // Create a new window that fills the screen minus the border
-    *win = newwin(new_height, new_width, 0, 0);
 
-    // Draw the border for the new window
-    wborder(*win, '|', '|', '-', '-', '+', '+', '+', '+');
+    // * Get the terminal dimensions
+    int term_height, term_width;
+    getmaxyx(stdscr, term_height, term_width);
+
+    // * Handle terminal resize
+    if (KEY_RESIZE) {
+        // ! Ensure the window dimensions are positive and fit within the screen
+        *height = term_height;
+        *width = term_width;
+
+        // * Delete the old window
+        if (*win != NULL) {
+            delwin(*win);
+        }
+
+        // * Create a new window that fills the screen
+        *win = newwin(*height, *width, 0, 0);
+
+        // * Draw the border for the new window
+        wborder(*win, '|', '|', '-', '-', '+', '+', '+', '+');
+    }
+
+    switch (*status) {
+        case 0: { // * Initial state
+            const char *message = "Press S to start or Q to quit";
+            int msg_length = strlen(message);
+
+            mvwprintw(*win, term_height / 2, (term_width - msg_length) / 2, "%s", message);
+            // TODO: The caller should handle key inputs ('S' or 'Q')
+            sleep(3);
+            *status = 0;
+            break;
+        }
+        case 1: { // * Start game
+            const char *message = "Game is ready";
+            int msg_length = strlen(message);
+
+            mvwprintw(*win, term_height / 2, (term_width - msg_length) / 2, "%s", message);
+            // TODO: The caller should handle key inputs ('S' or 'Q')
+            sleep(3);
+            *status = 0;
+            break;
+        }
+        default:
+            // TODO: Add more cases for different statuses as needed
+                break;
+    }
 
     // Refresh the standard screen and the new window
     wrefresh(stdscr);
@@ -69,7 +146,6 @@ int main(int argc, char *argv[]) {
      * @param argv[N+1..2N-1]: Write file descriptors (excluding keyboard_manager)
      * @param argv[2N]: Watchdog PID
     */
-
     if (argc != 2 * NUM_CHILDREN_WITH_PIPES + 1) {
         fprintf(stderr, "Usage: %s <read_fd1> <read_fd2> ... <read_fdN> <write_fd1> ... <write_fdN-1> <watchdog_pid>\n",
                 argv[0]);
@@ -84,57 +160,33 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // * Remap the read and write file descriptors
-    const int keyboard = read_fds[0];
-    const int obstacle_read = read_fds[1];
-    const int target_read = read_fds[2];
-    const int dynamic_read = read_fds[3];
+    // * Initialise window's game
+    if (initialize_ncurses() != EXIT_FAILURE) {
+        // * Game variables
+        WINDOW *win = NULL;
+        int height = 0;
+        int width = 0;
+        int status = 0;
+        int drone_pos[2] = {0, 0};
+        int drone_force[2] = {0, 0};
+        char *c;
+        do {
+            if (read(keyboard, c, 1) == -1 && !(errno == EAGAIN || errno == EWOULDBLOCK)) {
+                // * An actual error occurred
+                perror("read");
+                break;
+            }
+            game_window(&win, &height, &width, &status);
+        } while (!(status == -2  && *c == 'q')); // * Exit on 'q' and if status is -2
 
-    const int obstacle_write = write_fds[0];
-    const int target_write = write_fds[1];
-    const int dynamic_write = write_fds[2];
-
-    /*
-     * Initialization and setup of the map:
-     * - Open a new ncurses window;
-     * - Deactivate mouse support;
-     * - Delete the echo of the keys;
-     * - Delete the cursor highlight.
-    */
-    WINDOW *win = NULL;
-    int ch;
-
-    if (initscr() == NULL) {
-        fprintf(stderr, "Error initializing ncurses.\n");
-        return EXIT_FAILURE;
+        // * Delete the window
+        delwin(win);
     }
-    // * Start color functionality
-    start_color();
-    init_pair(1, COLOR_RED, COLOR_BLACK);    // * Define color pair 1: red text on black background
-    init_pair(2, COLOR_GREEN, COLOR_BLACK); // * Define color pair 2: green text on black background
-    init_pair(3, COLOR_BLUE, COLOR_WHITE);  // * Define color pair 3: blue text on white background
-
-    // * Disable mouse, echo, and cursor
-    mousemask(0, NULL);
-    cbreak(); // * Disable line buffering
-    noecho(); // * Disable echoing of typed characters
-    keypad(stdscr, TRUE); // * Enable function keys
-    curs_set(0); // * Hide the cursor
-
-    // * Initial window setup
-    attron(COLOR_PAIR(1));
-    // Initial setup of the window
-    resize_window(&win);
-    attroff(COLOR_PAIR(1));
-
-    while ((ch = getch()) != 'q') { // Exit on 'q'
-        if (ch == KEY_RESIZE) {     // Handle terminal resize
-            resize_window(&win);
-        }
+    else {
+        fprintf(stderr, "Error initializing ncurses.\n");
     }
 
     // * Clean up
-    delwin(win); // * Delete the window
     endwin(); // * Close ncurses window
 
     // Close file descriptors
@@ -144,6 +196,8 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < NUM_CHILDREN_WITH_PIPES - 1; i++) {
         close(write_fds[i]);
     }
+
+    // TODO: Send a signal to the other 5 processes to close
 
     return EXIT_SUCCESS;
 }
